2eabf490fadf26203f1f94f5f853388af262cede
Frank Wang <broodstar_frank@hotmail.com>
2023-02-27 18:57:52 -0500

    fix styling of thumbnail, display multiple messages in thumbnail to fill up space

--- pages/thumbnail/[slug].tsx
+++ pages/thumbnail/[slug].tsx
@@ -3,21 +3,34 @@ import { MessageResponse } from "stream-chat";
 import styles from "../../styles/Thumbnail.module.css";
 
 interface Props {
-  message: MessageResponse;
+  messages: MessageResponse[];
 }
 
 export default function Thumbnail(props: Props) {
-  const user = props.message.user!;
+  const previewMessages = props.messages.slice(0, 5);
   return (
-    <main>
-      <Image
-        src={user.image as string}
-        alt={user.name!}
-        width={32}
-        height={32}
-        style={{ borderRadius: 50 }}
-      />
-      <p className={styles.text}>{props.message.text}</p>
+    <main className={styles.main}>
+      {previewMessages.map((message, index) => {
+        const sameUserAsPreviousMessage =
+          index && previewMessages[index - 1].user?.id === message.user?.id;
+        const Avatar = !sameUserAsPreviousMessage ? (
+          <Image
+            src={message.user?.image as string}
+            alt={message.user?.name!}
+            width={32}
+            height={32}
+            style={{ borderRadius: 50 }}
+          />
+        ) : (
+          <div style={{ width: 32, height: 32 }} />
+        );
+        return (
+          <div className={styles.message}>
+            {Avatar}
+            <p className={styles.text}>{message.text}</p>
+          </div>
+        );
+      })}
     </main>
   );
 }
@@ -25,12 +38,11 @@ export default function Thumbnail(props: Props) {
 export async function getServerSideProps(context: { query: any }) {
   const { slug } = context.query;
   const res = await fetch(
-    `${process.env.FIREBASE_FUNCTION_URL}/getThumbnailMessage?conversationId=${slug}`
+    `${process.env.FIREBASE_FUNCTION_URL}/getConversation?conversationId=${slug}`
   );
   const data = await res.json();
+
   return {
-    props: {
-      message: data,
-    },
+    props: data,
   };
 }
