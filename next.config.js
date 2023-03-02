74d9b242ba15e7cc9f9b2626156cbabbb22a3fe9
Frank Wang <broodstar_frank@hotmail.com>
2023-03-01 19:58:08 -0500

    redirect post link to conversation link

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     domains: ["hips.hearstapps.com", "firebasestorage.googleapis.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/p/:slug",
+        destination: "/c/:slug",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
